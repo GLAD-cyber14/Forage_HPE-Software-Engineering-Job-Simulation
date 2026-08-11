b5015b652ecc0645adae53b939b58d1a7cc1a37f
Gladwin Solomon <gladwinsolomon006@gmail.com>
2026-08-11 18:24:05 +0530

    Add files via upload

---
+++ Task 03/EmployeeManager.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeManager{
+    private Employees employees;
+
+    public EmployeeManager(){
+        employees = new Employees();
+        List<Employee> employeeList = new ArrayList<>();
+
+        employeeList.add(new Employee(1,"John","Smith","johnsmith@gmail.com","Developer"));
+        employeeList.add(new Employee(2,"Michael","Brown","michaelbrown@gmail.com","Tester"));
+        employeeList.add(new Employee(3,"Samuel","Benny","samuelbenny@gmail.com","Manager"));
+        employeeList.add(new Employee(4,"George","Lake","georgelake@gmail.com","Designer"));
+
+        employees.setEmployees(employeeList);
+    }
+    public Employees getEmployees() {
+        return employees;
+    }
+    public void addEmployee (Employee employee){
+        employees.getEmployees().add(employee);
+    }
+
+}
